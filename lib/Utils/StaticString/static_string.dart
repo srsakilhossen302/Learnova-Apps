5147f5243d7018ff808fd7f43e4f6d6b026944da
srsakilhossen302 <ce.sakilhossen302@gmail.com>
2025-11-26 20:54:49 +0600

    Add Sign In View String

--- lib/Utils/StaticString/static_string.dart
+++ lib/Utils/StaticString/static_string.dart
@@ -1,7 +1,15 @@
 class AppString{
-
-
-
+  ///==============>>>>>>>>>> Sign In View <<<<<<<<<<<===============///
+  static const String appsName = "Learnova" ;
+  static const String logIn = "Log In" ;
+  static const String student = "Student" ;
+  static const String teacher = "Teacher" ;
+  static const String email = "Email" ;
+  static const String password = "Password" ;
+  static const String forgotPassword = "Forgot password?" ;
+  static const String loginWithGoogle = "Login with Google" ;
+  static const String newToLearnova = "New to Learnova?" ;
+  static const String createAnAccount = "Create an Account" ;
 
 
 }
